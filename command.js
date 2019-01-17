146ca71b3f64cc5743e015c8cbd1c9f9ebb5f46e
alena shipilova <alena@shipilova.com>
2019-01-17 05:51:10 +0300

    next/prev/25sec

--- command.js
+++ command.js
@@ -1,19 +1,16 @@
-sendMessage = (tabInfo, command) => {
-    chrome.tabs.query(tabInfo, (tabs) => {
-        if (tabs.length) {
-            chrome.tabs.sendMessage(tabs[0].id, command);
-        }
+sendMessage = (tabInfo, command) => {chrome.tabs.query(tabInfo, (tabs) => {
+        if (tabs.length) {chrome.tabs.sendMessage(tabs[0].id, command);}
     });
 };
 
 changeTrack = (command) => {
     if (command == "nextTrack"
         || command == "prevTrack"
-        )
+        || command == "changeAudioTime")
     {
         sendMessage({url: "*://*.vk.com/*"},
             {command: command});
     }
 };
 
-chrome.commands.onCommand.addListener(changeTrack);
+chrome.commands.onCommand.addListener(change);
